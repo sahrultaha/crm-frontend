d281c841511fc52e914e6bade1d6093d5d118542
Alif <alif.rafiuddin@dst.com.bn>
2022-11-08 11:00:13 +0800

    Remove dashboard.js and any links to it

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
             <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div className="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     {user ?
-                        <Link href="/dashboard">
+                        <Link href="/customers">
                             <a className="ml-4 text-sm text-gray-700 underline">
-                                Dashboard
+                                Customers
                             </a>
                         </Link>
                         :
