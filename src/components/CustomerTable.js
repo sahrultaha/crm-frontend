5c12c2514ab8aec58eac0791ad605f2e7205dfd0
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-09 16:04:35 +0800

    CustomerTable component

---
+++ src/components/CustomerTable.js
@@ -0,0 +1,73 @@
+import  TimestampDiv  from "./TimestampDiv.js";
+
+const CustomerTable = props => {
+    const data = props.data || []
+    if (props.isLoading) return 'Loading...'
+    if (data.length === 0) return 'Empty result.'
+    return (
+        <div className="mt-4 -mb-3" >
+            <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25 mb-5">
+                <div className="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
+                <div className="relative rounded-xl overflow-auto">
+                    <div className="shadow-sm overflow-hidden my-8">
+                        <table className="border-collapse table-auto w-full text-sm">
+                            <thead>
+                                <tr>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        ID
+                                    </th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        Name
+                                    </th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        Account Category
+                                    </th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        Email
+                                    </th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        Mobile
+                                    </th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        IC Number
+                                    </th>
+                                    <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                        Created At
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white dark:bg-slate-800">
+                                {data.map(customer => (
+                                <tr key={customer.id}>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{customer.id}</td>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+                                        {customer.name}
+                                    </td>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                                        {customer.account_category.name}
+                                    </td>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                                        {customer.email}
+                                    </td>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                                        {customer.mobile_number}
+                                    </td>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                                        {customer.ic_number}
+                                    </td>
+                                    <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                                       <TimestampDiv time={customer.created_at} />
+                                    </td>
+                                </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"></div>
+            </div>
+        </div>
+    )
+}
+
+export default CustomerTable
