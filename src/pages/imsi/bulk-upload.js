a1552e3fa772f9181a8b0709aecc07f725405eb4
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-11 10:42:43 +0800

    use a tag

--- src/pages/imsi/bulk-upload.js
+++ src/pages/imsi/bulk-upload.js
@@ -46,7 +46,7 @@ const BulkUpload = () => {
                 </div>
                 <div className="flex flex-row">
                     <Link href="/templates/bulk-imsi-template.csv">
-                        <div className="flex-shrink-0 flex items-center hover:text-gray-700 hover:border-gray-700">
+                        <a className="flex-shrink-0 flex items-center border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300" id='file-template'>
                             File Template
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +61,7 @@ const BulkUpload = () => {
                                     d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
                                 />
                             </svg>
-                        </div>
+                        </a>
                     </Link>
                 </div>
             </MainBody>
