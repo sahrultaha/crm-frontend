0f42044d36590028442c10886d3c0a475ce2f13f
Alif <alif.rafiuddin@dst.com.bn>
2022-11-18 11:33:10 +0800

    Add basic ui for creating a new subscription

--- src/pages/subscriptions/create.js
+++ src/pages/subscriptions/create.js
@@ -1,6 +1,8 @@
 import AppLayout from '@/components/Layouts/AppLayout'
+import Button from '@/components/Button'
 import Head from 'next/head'
 import { useState } from 'react'
+import axios from '@/lib/axios'
 import MainBody from '@/components/MainBody'
 import IcCheckingInputsLite from '@/components/forms/subscriptions/IcCheckingInputsLite'
 import SearchPackByNumber from '@/components/forms/subscriptions/SearchPackByNumber'
@@ -8,8 +10,37 @@ import SearchPackByNumber from '@/components/forms/subscriptions/SearchPackByNum
 const Create = () => {
     const [customer, setCustomer] = useState({})
     const [pack, setPack] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [subCreated, setSubCreated] = useState(false)
 
     const isEmptyObject = c => Object.keys(c).length === 0
+
+    const createSubscription = _ => {
+        if (loading) return
+        if (isEmptyObject(pack)) return
+
+        setLoading(true)
+        axios
+            .post('/api/subscriptions', {
+                customer_id: customer.id,
+                registration_date: '2022-11-01',
+                subscription_status_id: 1,
+                subscription_type_id: 1,
+                number_id: pack.number_id,
+                imsi_id: pack.imsi_id,
+                activation_date: '2022-11-01',
+            })
+            .then(resp => {
+                console.log('Subscription created...', resp)
+                setSubCreated(true)
+            })
+            .catch(e => {
+                console.error('Something went wrong...', e)
+            })
+            .finally(_ => {
+                setLoading(false)
+            })
+    }
 
     return (
         <AppLayout
@@ -35,7 +66,7 @@ const Create = () => {
                     ''
                 )}
 
-                {!isEmptyObject(customer) ? (
+                {!isEmptyObject(customer) && isEmptyObject(pack) ? (
                     <SearchPackByNumber onPackChange={setPack} />
                 ) : (
                     ''
@@ -46,6 +77,16 @@ const Create = () => {
                         <p>Pack id is {pack.id}</p>
                         <p>Product id is {pack.product_id}</p>
                         <p>Imsi id is {pack.imsi_id}</p>
+                        {!loading && !subCreated && (
+                            <Button onClick={createSubscription}>
+                                Create Subscription
+                            </Button>
+                        )}
+                        {!loading && subCreated && (
+                            <p className="text-green-500">
+                                A new subscription has been created.
+                            </p>
+                        )}
                     </div>
                 ) : (
                     ''
