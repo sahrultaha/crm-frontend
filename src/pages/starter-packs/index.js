7b5ae682381b503862886bfd307958cff17f2c96
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-23 10:12:07 +0800

    use MUI pagination

--- src/pages/starter-packs/index.js
+++ src/pages/starter-packs/index.js
@@ -4,8 +4,9 @@ import MainBody from '@/components/MainBody'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import axios from '@/lib/axios'
+import { Pagination } from '@mui/material'
 
-const PackTable = ({ isLoading, items, ...props }) => {
+const PackTable = ({ isLoading, items }) => {
     if (isLoading) {
         return <div>Loading....</div>
     }
@@ -17,57 +18,61 @@ const PackTable = ({ isLoading, items, ...props }) => {
     }
 
     return (
-        <table>
+        <table className="border-collapse table-auto w-full text-sm">
             <thead>
                 <tr>
-                    <th>id</th>
-                    <th>msisdn</th>
-                    <th>imsi</th>
-                    <th>plan</th>
-                    <th>Activation Date</th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                        id
+                    </th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                        msisdn
+                    </th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                        imsi
+                    </th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                        plan
+                    </th>
+                    <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                        Activation Date
+                    </th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody className="bg-white dark:bg-slate-800">
                 {items.map(item => (
                     <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.number.number}</td>
-                        <td>{item.imsi.imsi}</td>
-                        <td>{item.product.name}</td>
-                        <td>{item.installation_date}</td>
+                        <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                            {item.id}
+                        </td>
+                        <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                            {item.number.number}
+                        </td>
+                        <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                            {item.imsi.imsi}
+                        </td>
+                        <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                            {item.product.name}
+                        </td>
+                        <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                            {item.installation_date}
+                        </td>
                     </tr>
                 ))}
             </tbody>
         </table>
     )
 }
-const PackNavigation = ({ meta, pageHandler }) => {
-    if (!meta || meta === undefined) {
-        return <div></div>
-    }
-    const pageLinks = []
 
-    for (let i = 1; i <= meta.last_page; i++) {
-        pageLinks.push(
-            <li
-                key={i}
-                id={'page-link-' + i}
-                className="mr-2 cursor-pointer"
-                onClick={() => pageHandler(i)}>
-                {i}
-            </li>,
-        )
-    }
-    return <ul>{pageLinks}</ul>
-}
 const Dashboard = () => {
     const router = useRouter()
     const [loading, setLoading] = useState(true)
-    const [meta, setMeta] = useState()
+    const [meta, setMeta] = useState({ current_page : 1, last_page : 1})
     const [items, setItems] = useState([])
+    const [page, setPage] = useState(1)
 
     const pageHandler = i => {
-        console.log(i)
+        console.log(arguments) // outputs the arguments to the console
+        setPage(i)
     }
 
     const makePaginationUrl = ({ url, page, limit, sort }) => {
@@ -83,10 +88,11 @@ const Dashboard = () => {
     useEffect(() => {
         if (!router.isReady) return
         axios
-            .get(makePaginationUrl({ url: 'api/packs' }))
+            .get(makePaginationUrl({ url: 'api/packs', page: page }))
             .then(resp => {
                 setItems(resp.data.data)
                 setMeta(resp.data.meta)
+                console.log(resp.data.meta)
             })
             .catch(e => {
                 console.error('error fetching data', e)
@@ -94,16 +100,29 @@ const Dashboard = () => {
             .finally(() => {
                 setLoading(false)
             })
-    }, [router.isReady])
+    }, [router.isReady, page])
 
     const pages = []
 
     return (
         <div>
-            <PackTable isLoading={loading} items={items}></PackTable>
-            <PackNavigation
-                meta={meta}
-                pageHandler={pageHandler}></PackNavigation>
+            <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25 mb-5">
+                <div className="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
+                <div className="relative rounded-xl overflow-auto">
+                    <div className="shadow-sm overflow-hidden my-8">
+                        <PackTable
+                            isLoading={loading}
+                            items={items}
+                            className="border-collapse table-auto w-full text-sm"
+                            tdClassName="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left"></PackTable>
+                    </div>
+                </div>
+            </div>
+            <Pagination
+                page={meta.current_page}
+                defaultPage={meta.current_page}
+                count={meta.last_page}
+                onChange={(e, page) => pageHandler(page)}></Pagination>
         </div>
     )
 }
